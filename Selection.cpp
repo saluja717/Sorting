9a94aa8c92742ff4c2300153cdead37cf4a0f713
saluja717 <68748154+saluja717@users.noreply.github.com>
2020-12-15 11:25:56 +0530

    Add files via upload

---
+++ Selection.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+void selection_sort(int a[],int n)
+{
+     int c;
+      for(int i=0;i<n-1;i++)
+      {
+          c=i;
+          for(int j=i+1;j<n;j++)
+          {
+              if(a[j]<a[c])
+              {
+                  c=j;
+              }
+          }
+          swap(a[i],a[c]);
+      }
+      for(int i=0;i<n;i++)
+      {
+          cout<<a[i]<<" ";
+      }
+}
+int main()
+{
+      int n;
+      cin>>n;
+      int a[n];
+      for(int i=0;i<n;i++)
+      {
+          cin>>a[i];
+      }
+      selection_sort(a,n);
+      return 0;
+}
