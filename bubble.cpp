7e9d9cdaab672eb24fa8edc15b993a080cfa7029
saluja717 <68748154+saluja717@users.noreply.github.com>
2020-12-15 11:23:48 +0530

    Add files via upload

---
+++ bubble.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define l long long 
+void bubble_sort(int a[],int n)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-1-i;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                int temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+}
+int main()
+{
+       int n;
+      cin>>n;
+       int a[n];
+      for( int i=0;i<n;i++)
+      {
+          cin>>a[i];
+      }
+      bubble_sort(a,n);
+       return 0;
+}     
